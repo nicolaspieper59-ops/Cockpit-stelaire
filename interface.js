a6e1f8c1d6fecb3f791edbb54b27cc297263dbd6
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-09-21 15:55:54 +0000

    Update interface.js

--- interface.js
+++ interface.js
@@ -2,21 +2,18 @@ import { modules } from './modules/loader.js';
 
 window.onload = () => {
   const panel = document.getElementById('panel');
-  modules.forEach(mod => {
-    const btn = document.createElement('button');
-    btn.textContent = mod.name;
-    btn.onclick = () => {
-      fetch(`modules/${mod.file}.html`)
-        .then(res => res.text())
-        .then(html => {
-          const container = document.createElement('div');
-          container.className = 'module';
-          container.innerHTML = html;
-          panel.appendChild(container);
-          import(`./modules/${mod.file}.js`).then(m => m.activate(container));
-        });
-    };
-    panel.appendChild(btn);
-  });
+
+  // Charger automatiquement le module "cockpit-universel"
+  const mod = modules.find(m => m.file === 'cockpit-universel');
+  if (mod) {
+    fetch(`modules/${mod.file}.html`)
+      .then(res => res.text())
+      .then(html => {
+        const container = document.createElement('div');
+        container.className = 'module';
+        container.innerHTML = html;
+        panel.appendChild(container);
+        import(`./modules/${mod.file}.js`).then(m => m.activate(container));
+      });
+  }
 };
-            
